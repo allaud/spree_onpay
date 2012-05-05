82ab98e56df97282f04c43f84ecfedf8275dc757
RuslanRoot <ruslan.root@gmail.com>
2012-05-05 19:54:39 +0300

    prepare for spree 1.1.0

--- lib/spree_onpay.rb
+++ lib/spree_onpay.rb
@@ -5,13 +5,16 @@ module SpreeOnpay
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-        Rails.env.production? ? require(c) : load(c)
-      end
-      Gateway::Onpay.register
+    # def self.activate
+    #   Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+    #     Rails.env.production? ? require(c) : load(c)
+    #   end
+    #    Spree::Gateway::Onpay.register
+    # end
+    initializer "spree.gateway.payment_methods", :after => "spree.register.payment_methods" do |app|
+      app.config.spree.payment_methods << Spree::Gateway::Onpay
     end
 
-    config.to_prepare &method(:activate).to_proc
+    #config.to_prepare &method(:activate).to_proc
   end
 end
